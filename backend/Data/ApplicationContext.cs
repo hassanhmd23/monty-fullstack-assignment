5ead5cb7e7cf5ade5357beb5d147fea8cdf8407f
hassanhmd23 <hassanhmd1734@gmail.com>
2024-04-06 16:36:25 +0300

    Remove Commented code

--- backend/Data/ApplicationContext.cs
+++ backend/Data/ApplicationContext.cs
@@ -1,7 +1,5 @@
 using backend.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Data
@@ -16,28 +14,6 @@ namespace backend.Data
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<User>().ToTable("Users");
-            // List<IdentityRole> roles = new()
-            // {
-            //     new() {
-            //         Name = "Admin",
-            //         NormalizedName = "ADMIN"
-            //     },
-            //     new() {
-            //         Name = "User",
-            //         NormalizedName = "USER"
-            //     }
-            // };
-            // modelBuilder.Entity<IdentityRole>().HasData(roles);
-            // var hasher = new PasswordHasher<User>();
-            // var user = new User
-            // {
-            //     Id = "01554538-afc4-4acc-9ac9-0aef00260a23",
-            //     UserName = "admin",
-            //     Email = "admin@admin.com",
-            //     EmailConfirmed = true,
-            // };
-            // user.PasswordHash = hasher.HashPassword(user, "Admin123@");
-            // var createdUser = modelBuilder.Entity<User>().HasData(user);
         }
         public DbSet<Subscription> Subscriptions { get; set; }
     }
