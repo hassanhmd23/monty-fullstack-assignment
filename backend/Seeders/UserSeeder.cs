ea457e5fe2d185ac3d5d5bc89eb61934fb625e10
hassanhmd23 <hassanhmd1734@gmail.com>
2024-04-09 20:38:09 +0300

    Add more users seeders

--- backend/Seeders/UserSeeder.cs
+++ backend/Seeders/UserSeeder.cs
@@ -14,10 +14,31 @@ namespace backend.Seeders
             var user = await userManager.FindByNameAsync("admin");
             if (user == null)
             {
-                user = new User { UserName = "admin", Email = "admin@admin.com", FirstName = "Admin", LastName = "Admin", Role = "Admin", Gender = "Male", Country = "Lebanon" };
+                user = new User { UserName = "admin", Email = "hassanhmd1734@gmail.com", FirstName = "Hassan", LastName = "Hammoud", Role = "Admin", Gender = "Male", Country = "Lebanon" };
                 await userManager.CreateAsync(user, "Admin123@");
-                await userManager.AddToRoleAsync(user, "Admin");
+                await userManager.AddToRoleAsync(user, user.Role);
+
+                for (var i = 0; i < 15; i++)
+                {
+                    user = new User
+                    {
+                        UserName = Faker.Internet.UserName(),
+                        Email = Faker.Internet.Email(user.UserName),
+                        FirstName = Faker.Name.First(),
+                        LastName = Faker.Name.Last(),
+                        Role = "User",
+                        Gender = Faker.RandomNumber.Next(0, 1) == 1 ? "Male" : "Female",
+                        Country = Faker.Address.Country()
+                    };
+                    await userManager.CreateAsync(user, "User123@");
+                    await userManager.AddToRoleAsync(user, user.Role);
+                }
             }
+        }
+        private enum Gender
+        {
+            Male,
+            Female
         }
     }
 }
